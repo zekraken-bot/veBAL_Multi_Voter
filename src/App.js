325584ace9c6b05c5eef84847248f092173ef600
ZeKraken <zekraken00@gmail.com>
2022-11-22 23:32:03 -0500

    update 11_22_22_v2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import './App.css';
 import { useEffect, useState } from "react";
 import { ethers } from 'ethers';
 import {Vote_ABI, Vote_address} from './abi';
+import Button from '@mui/material/Button';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import { styled } from "@mui/material/styles";
+
 
 
 export default function App() {
@@ -14,28 +20,73 @@ export default function App() {
   const [data, setData] = useState()
   const apiURL = 'https://raw.githubusercontent.com/balancer-labs/frontend-v2/master/public/data/voting-gauges.json'
   let displayData
-    
-  let handleGaugeChange1 = (e) => {
-    setGauge1(e.target.value)
-  }
-  let handleGaugeChange2 = (e) => {
-    setGauge2(e.target.value)
-  }
+  
+  const theme = createTheme({
+    status: {
+      danger: '#e53e3e',
+    },
+    palette: {
+      neutral: {
+        main: '#64748B',
+        contrastText: '#ffffff',
+      },
+      red: {
+        main: '#9c2238',
+        contrastText: '#ffffff',
+      }
+    },
+  });
+  
+  const StyledAutocomplete = styled(Autocomplete)({
+    "& .MuiInputLabel-outlined": {
+      color: "#dbdbdb"
+    },
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: "white"
+      },
+      '&:hover fieldset': {
+        borderColor: "#0f6ea6",
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: "#0f97a6",
+      }
+    },
+    '& .MuiOutlinedInput-input' : {
+      color: "#dbdbdb",
+    }
+  })
+
   const helper =['hi']
   async function pullJSON() {
   const response = await fetch(apiURL)
   const responseData = await response.json()
+    //console.log(responseData)
     
     displayData = helper.map((item) => {
       return(
-          <div key={"maindiv"}>       
-          <select className='dropdown' onChange={handleGaugeChange1}> <option disabled selected value> ----- select a guage you want to be 0% from the dropdown list ----- </option>
-          {responseData.map((gauge1) => <option value={gauge1.address.slice(2,42)} key={gauge1.address}>{[gauge1.pool.symbol," - ",gauge1.address," - gauge cap:",gauge1.relativeWeightCap]}</option>)}
-          </select>
-          <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>     
-          <select className='dropdown' onChange={handleGaugeChange2}> <option disabled selected value> ----- select a guage you want to be 100% from the dropdown list ----- </option>
-          {responseData.map((gauge2) => <option value={gauge2.address.slice(2,42)} key={gauge2.address}>{[gauge2.pool.symbol," - ",gauge2.address," - gauge cap:",gauge2.relativeWeightCap]}</option>)}
-          </select>
+          <div className="autoComplete" key={"maindiv"}>       
+                  
+          <StyledAutocomplete
+            onChange={(event, newValue) => {
+              setGauge1(newValue.address.slice(2,42))
+            }}
+            id="first_address_box"
+            options={responseData}
+            getOptionLabel={(option) => JSON.stringify([option.pool.symbol,option.address,option.relativeWeightCap])}
+            sx={{ width: 800 }}
+            renderInput={(params) => <TextField {...params} label="Address" />}
+          />
+          <StyledAutocomplete
+            onChange={(event, newValue2) => {
+              setGauge2(newValue2.address.slice(2,42))
+            }}
+            id="second_address_box"
+            options={responseData}
+            getOptionLabel={(option) => JSON.stringify([option.pool.symbol,option.address,option.relativeWeightCap])}
+            sx={{ width: 800 }}
+            renderInput={(params) => <TextField {...params} label="Address" variant="outlined"/>}
+          />
           </div>
       )
     })
@@ -86,11 +137,16 @@ export default function App() {
     await erc20.vote_for_many_gauge_weights([gauge1,gauge2,"0x0000000000000000000000000000000000000000","0x0000000000000000000000000000000000000000","0x0000000000000000000000000000000000000000","0x0000000000000000000000000000000000000000","0x0000000000000000000000000000000000000000","0x0000000000000000000000000000000000000000"],[0,10000,0,0,0,0,0,0]);
   }
 
+  
+
   return (
+    <ThemeProvider theme={theme}>
     <div>
       <header className="headerContent">
       <br />
-      <p align="right"><button onClick={requestAccount}>{buttonText}</button></p>
+      
+      <p align="right"><Button variant="contained" color="neutral" onClick={requestAccount}>{buttonText}</Button></p>
+      
       <p align="right">Wallet Address: {walletAddress}</p>
       </header>  
       
@@ -104,15 +160,19 @@ export default function App() {
         <li>If there are two gauges in the list select the gauge with a cap</li>
         <li>Current functionality only works for voting 0% for 1st gauge and 100% for the 2nd gauge</li>
         <li>Hex data has been included if you want to inspect or send directly to the gaugeController contract</li>
+        <li>Info in dropdown 1) gauge symbol, 2) gauge address, 3) gauge cap</li>
       </p>
       <br />
       {data}
       <br />
-      <button className ="button" onClick={updateVotes}>Click to vote for veBAL gauges</button>
+      <Button variant="contained" color="red" className ="button" onClick={updateVotes}>Click to vote for veBAL gauges</Button>
+      
       <br />
       <p className="hexData">HEX Data:<br /><br />0x2e4e99a1000000000000000000000000{gauge1}000000000000000000000000{gauge2}00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002710000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
       </p>
       </div>
+      
     </div>
+    </ThemeProvider>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { StyledEngineProvider } from '@mui/material/styles';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -8,7 +9,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <StyledEngineProvider injectFirst>
+      <App />
+    </StyledEngineProvider>
   </React.StrictMode>
 );
 
