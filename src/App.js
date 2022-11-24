0378b0cd321825ec4a986419feebe71c00d5de6f
ZeKraken <zekraken00@gmail.com>
2022-11-24 12:10:07 -0500

    update 11_24_22

--- src/App.js
+++ src/App.js
@@ -65,9 +65,8 @@ export default function App() {
     displayData = (() => {
       return(
           <div>       
-          <Grid sx={{ flexGrow: 1 }} container spacing={2}>       
+          <Grid container spacing={2}>       
           <Grid item xs={6}>
-
           <StyledAutocomplete
             onChange={(event, newValue) => {
               setGauge1(newValue.address.slice(2,42))
@@ -75,7 +74,7 @@ export default function App() {
             id="first_address_box"
             options={responseData}
             getOptionLabel={(option) => JSON.stringify([option.pool.symbol,option.address,option.relativeWeightCap])}
-            sx={{ width: 800 }}
+            sx={{ maxWidth: 800 }}
             renderInput={(params) => <TextField {...params} label="Gauge Address => 0%" />}
           />
           </Grid>
@@ -87,7 +86,7 @@ export default function App() {
             id="second_address_box"
             options={responseData}
             getOptionLabel={(option) => JSON.stringify([option.pool.symbol,option.address,option.relativeWeightCap])}
-            sx={{ width: 800 }}
+            sx={{ maxWidth: 800 }}
             renderInput={(params) => <TextField {...params} label="Grid Address => 100%"/>}
           />
           </Grid>
@@ -98,7 +97,6 @@ export default function App() {
     setData(displayData)
   }
   
-  //useEffect() loads when page is first loaded
   useEffect(() => {
     pullJSON()
     checkWalletonLoad()
@@ -143,42 +141,42 @@ export default function App() {
     await erc20.vote_for_many_gauge_weights([gauge1,gauge2,"0x0000000000000000000000000000000000000000","0x0000000000000000000000000000000000000000","0x0000000000000000000000000000000000000000","0x0000000000000000000000000000000000000000","0x0000000000000000000000000000000000000000","0x0000000000000000000000000000000000000000"],[0,10000,0,0,0,0,0,0]);
   }
 
-  
-
   return (
     <ThemeProvider theme={theme}>
-    <div>
+    <>
       <header className="headerContent">
-      <br />
-      
+      <br />      
       <p align="right"><Button variant="contained" color="neutral" onClick={requestAccount}>{buttonText}</Button></p>
-      
       <p align="right">Wallet Address: {walletAddress}</p>
       </header>  
       
       <div className="mainContent">
-      <p className="vebaltitle">
-        <u>veBAL Gauge Voter</u>
-      </p>
-      <p>
-        Usage Notes:
-        <li>Double check your wallet address before voting</li>
-        <li>If there are two gauges in the list select the gauge with a cap</li>
-        <li>Current functionality only works for voting 0% for 1st gauge and 100% for the 2nd gauge</li>
-        <li>Hex data has been included if you want to validate and/or send directly to the gaugeController contract</li>
-        <li>Info in dropdown 1) gauge symbol, 2) gauge address, 3) gauge cap</li>
-      </p>
-      <br />
-      {data}
-      <br />
-      <Button variant="contained" color="red" className ="button" onClick={updateVotes}>Click to vote for veBAL gauges</Button>
-      
-      <br />
-      <p className="hexData">HEX Data:<br /><br />0x2e4e99a1000000000000000000000000{gauge1}000000000000000000000000{gauge2}00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002710000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
-      </p>
+        <div className='row'>
+          <div className='flex-item'>
+            <p className="vebaltitle">
+            <u>veBAL Gauge Voter</u>
+            </p>
+          </div>
+          <p>
+            Usage Notes:
+            <li>Double check your wallet address before voting</li>
+            <li>If there are two gauges in the list select the gauge with a cap</li>
+            <li>Current functionality only works for voting 0% for 1st gauge and 100% for the 2nd gauge</li>
+            <li>Hex data has been included if you want to validate and/or send directly to the gaugeController contract</li>
+            <li>Info in dropdown 1) gauge symbol, 2) gauge address, 3) gauge cap</li>
+          </p>
+          <br />
+          <div className='flex-item'>
+          {data}          
+          <br />          
+          <Button variant="contained" color="red" className ="button" onClick={updateVotes}>Click to vote for veBAL gauges</Button>          
+          <br />
+          <p className="hexData">HEX Data:<br /><br />0x2e4e99a1000000000000000000000000{gauge1}000000000000000000000000{gauge2}00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002710000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+          </p>
+          </div>
+        </div>
       </div>
-      
-    </div>
+    </>
     </ThemeProvider>
   );
 }
